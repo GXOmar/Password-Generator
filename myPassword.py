3725a1d6826ceb678c40e4dbc07c2c7b3ed720d2
Omar Shaikh Sobuh <pysoftengineer@hotmail.com>
2022-07-25 17:42:44 +0300

    fixing typos in the script and welcome_message.txt

--- myPassword.py
+++ myPassword.py
@@ -1,13 +1,19 @@
 #! python3
-# This Program is a text based experience to Generate a Random password for the user.
-# and also to ask the user to enter a password to check if it's strong or not.
 
-# a strong password should contain the following:
-# 1 >>> at least 8 characters long.
-# 2 >>> at least 1 upper 
-# 3 >>> at least 1 lower case letter.
-# 4 >>> at least 1 digit.
-# 5 >>> at least 1 symbol
+# myPassword.py - Password generator and validator
+
+# This program is a text-based experience to Generate a Random password for the user.
+# and validate a password from user input.
+
+# USAGE: py myPassword.py <command_keyword> [optional_password_length]
+# see welcome_message.txt for more info on how to use it...
+
+# A strong password should contain the following:
+# 1- At least 8 characters long.
+# 2- At least 1 lowercase character.
+# 3- At least 1 uppercase character.
+# 4- At least 1 digit.
+# 5- At least 1 symbol.
 
 import re
 import sys
@@ -17,145 +23,140 @@ import secrets
 import colorama
 import pyperclip
 
-# create a Regex to check the password first.
-detectPW = re.compile(r'\S+')
+# A regex to detect string sequences.
+detect_password = re.compile(r'\S+')
 
-# create a Regex to check if the password strong or not.
-StrongPW = re.compile(r'''^
-(?=.*[a-z])                          # at least one lower case letter
-(?=.*[A-Z])                          # at least one upper case letter
-(?=.*[0-9])                          # at least one digit
-(?=.*[!?@#$%^&*])                    # at least one symbol
-# (?!.*[\[\]():;,~.`'"/+=\\-_<>{}])  # make sure the PW doesn't include these characters
-\S{8,}$''', re.VERBOSE)
+# A regex to check if the password is strong or not.
+detect_strong_password = re.compile(r"""^
+(?=.*[a-z])                          # at least 1 lowercase character
+(?=.*[A-Z])                          # at least 1 uppercase character
+(?=.*[0-9])                          # at least 1 digit
+(?=.*[!?@#$%^&*])                    # at least 1 symbol
+# (?!.*[\[\]():;,~.`'"/+=\\-_<>{}])  # make sure the password doesn't include these characters
+\S{8,}$""", re.VERBOSE)
 
-# DONE: Generate a Random PASSWORD 
-# DONE: let the user choose the length of the password
-# DONE: make a choice for the user to select the level of the password (weak, medium, strong or very strong)
-# DONE: copy the strong PW to the clipboard. 
-# DONE: make an Explanation why the Password is not strong, e.g. Missing a Number! 
-# _____________________________________________________ 
-# TODO: implement a GUI for this program 
-# TODO: make a strength bar to indecate the PW strength insted of "(weak, medium, strong or very strong)".
-# TODO: let the user choice one or multiple character types to generate a PW around it e.g. only symbols
-# TODO: find the unwanted character and point it out.
+# sequence of characters to generate a random password
+LOWERCASE_CHARACTERS = string.ascii_lowercase
+UPPERCASE_CHARACTERS = string.ascii_uppercase
+DIGITS = string.digits
+SYMBOLS = "!?@#$%^&*"
+ALL_CHARACTERS = LOWERCASE_CHARACTERS + UPPERCASE_CHARACTERS + DIGITS + SYMBOLS
 
-# sequence of characters for the random password
-lower_case, upper_case = string.ascii_lowercase, string.ascii_uppercase
-digit, symbols = string.digits, '!?@#$%^&*'
-all = lower_case + upper_case + digit + symbols
-
-R_lowercase, R_uppercase = secrets.choice(lower_case), secrets.choice(upper_case)
-R_digit, R_symbols = secrets.choice(digit), secrets.choice(symbols) 
-R_all = R_lowercase + R_uppercase + R_digit + R_symbols # eqaul to 4 character
+# guaranteed at least one character from each category when generating a random password.
+# this eliminates the randomness of NOT having at least one of each character.
+guarantee_lowercase = secrets.choice(LOWERCASE_CHARACTERS)
+guarantee_uppercase = secrets.choice(UPPERCASE_CHARACTERS)
+guarantee_digit = secrets.choice(DIGITS)
+guarantee_symbols = secrets.choice(SYMBOLS) 
+guarantee_all = guarantee_lowercase + guarantee_uppercase + guarantee_digit + guarantee_symbols  # equal to 4 character
 
 # guaranteed Random characters
 Random_character_values = {
-    'weak' : R_lowercase,
-    'medium' : R_lowercase + R_uppercase,
-    'strong' : R_lowercase + R_uppercase + R_digit,
-    'very strong' : R_all
-    # 'symbols' : R_symbols
+    "weak": guarantee_lowercase,
+    "medium": guarantee_lowercase + guarantee_uppercase,
+    "strong": guarantee_lowercase + guarantee_uppercase + guarantee_digit,
+    "very strong": guarantee_all
 }
 
 # ask the user for the password strength (weak, medium, strong or very strong)
-# each key(string) is an optional user input that represent PW strength(category of character), strong by default.
+# each key(string) is an optional user input that represents PW strength(category of character), strong by default.
 PWstrength_chars = {
-    'weak': lower_case,
-    'medium' : lower_case + upper_case,
-    'strong' : lower_case + upper_case + digit, 
-    'very strong' : all, 
-    # 'symbols' : symbols
+    "weak": LOWERCASE_CHARACTERS,
+    "medium": LOWERCASE_CHARACTERS + UPPERCASE_CHARACTERS,
+    "strong": LOWERCASE_CHARACTERS + UPPERCASE_CHARACTERS + DIGITS, 
+    "very strong": ALL_CHARACTERS
 } 
     
-def GeneratePW(length, PW_strength):
-    '''this function will generate a random password and return it'''
+def generate_random_password(length: int, pw_strength: str) -> str:
+    """Generate a random password"""
     # error handling: length of PW should not be < 8
     if length < 8: 
         length = 8
     # First generate length-Random_sequence of characters in a list minus the length of guaranteed Random characters. 
     # e.g. 12 - 4(R_all) = 8 so will generate 8 random character.
-    Random_PW = [secrets.choice(PWstrength_chars[PW_strength]) for _ in range(0, length - len(Random_character_values[PW_strength]))]
+    Random_PW = [secrets.choice(PWstrength_chars[pw_strength]) for _ in range(0, length - len(Random_character_values[pw_strength]))]
     # we add the guaranteed Random characters so we make sure the randomly generated PW include what the user asked for.
     # e.g. add the last 4(from R_all) remaining character so we get 12 character back.
-    Random_PW.extend(Random_character_values[PW_strength])
+    Random_PW.extend(Random_character_values[pw_strength])
     # shuffle the list so the added Random_characters don't stay in order.
     random.shuffle(Random_PW)
     return ''.join(Random_PW)
 
 
-def explain(PWstr):
-    '''Check what type of characters is missing from the password, return missing category(s)'''
+def explain(password_str: str) -> str:
+    """Check what type of characters is missing from the password, return missing category(s)"""
     li = ['an uppercase', 'a lowercase', 'a digit', 'a symbol']
-    for char in PWstr:
+    for char in password_str:
         li[0] = None if char.isupper() else li[0]
         li[1] = None if char.islower() else li[1]
         li[2] = None if char.isdigit() else li[2]
-        li[3] = None if char in '!?@#$%^&*' else li[3]
+        li[3] = None if char in "!?@#$%^&*" else li[3]
     
     li = [item for item in li if item != None]
     return f"{', '.join(li[:-1])} and {li[-1]}" if len(li) > 1 else f"{li[0]} character"
 
-def unwanted_ch_list(PWstr): 
-    '''Detect if PW has unwanted characters, return True if so'''
-    matched_list = [character in '\[\]():;,~.`\'\"/+=\\-_<>}{' for character in PWstr]   
-    if any(matched_list): # if one unwanted char found in PW, PW is invalid
+
+def unwanted_chr_in_password(password_str: str) -> bool: 
+    """Detect if the password has unwanted characters, return True if so"""
+    matched_list = [character in "\[\]():;,~.`\'\"/+=\\-_<>}{" for character in password_str]   
+    if any(matched_list):  # if one unwanted character is found in the password, the password is invalid
         return True
     return False
 
-def checkPW(user_password):
-    'check if the password is strong or not, return a message about the password'
-    # detect if Pw
-    PW = detectPW.search(user_password)
-    if PW != None: # means it return a 'PW' match
 
-        # search for Strong PW
-        SPW = StrongPW.search(PW.group())
+def check_password(user_password):
+    """Check if the password is strong or not, return a message about the password"""
+    # detect for a password sequence
+    PASSWORD_SEQUENCE = detect_password.search(user_password)
+    if PASSWORD_SEQUENCE != None:
 
-        # check for unwanted characters in PW
-        if unwanted_ch_list(user_password):
+        # check for unwanted characters in PASSWORD_SEQUENCE
+        if unwanted_chr_in_password(user_password):
             print(f"{colorama.Fore.RESET}Your password {colorama.Fore.YELLOW}\"{user_password}\"{colorama.Fore.RESET} contains unwanted characters")
             return False
 
-        # check the length of PW 
-        elif len(user_password) <= 6:
-            print(f'{colorama.Fore.LIGHTYELLOW_EX}Your password is too short!')
+        # check the length of PASSWORD_SEQUENCE 
+        elif len(user_password) <= 7:
+            print(f"{colorama.Fore.LIGHTYELLOW_EX}Your password is too short!")
             return False
 
-        # check if PW is strong
-        elif SPW != None: # return a 'strong PW' match
-            # hidePW = re.sub(StrongPW, SPW.group().replace(SPW.group()[:-5], len(SPW.group()[:-5]) * '*'), SPW.group()) # LOL
-            print(f"{colorama.Fore.GREEN}Your password '{user_password}' is strong")
+        # detect and validate the password
+        elif detect_strong_password.search(PASSWORD_SEQUENCE.group()) != None:  # if found it returns a strong password match
+            print(f"{colorama.Fore.GREEN}Your password \"{user_password}\" is strong")
             return True
 
         else:          
             print(f"{colorama.Fore.RED}Your Password is NOT strong, it's missing {colorama.Fore.MAGENTA}{explain(user_password)}.")
             return False
 
+    # TODO: handle no password detection
+
+
 def main():
-    # add welcome screen with instructions on how to use the script
-    command_keywords = ('gen', 'check')
-    if len(sys.argv) == 1 or sys.argv[1] not in command_keywords:
-        welcome_message = open('.\welcome_message.txt', 'r').read()
+    COMMAND_KEYWORDS = ("gen", "check")
+    if len(sys.argv) == 1 or sys.argv[1] not in COMMAND_KEYWORDS:
+        # add a welcome screen with instructions on how to use the script.
+        welcome_message = open(".\welcome_message.txt", "r").read()
         print(welcome_message)
 
     elif len(sys.argv) >= 2:
-        if sys.argv[1] == 'gen':
-        # generate a very strong random password with the 'gen' command with default length of 20 characters
+        if sys.argv[1] == "gen":
+        # generate a very strong random password with the "gen" command with a default length of 20 characters.
             try:
                 # user supplied a password length!
-                random_generated_password = GeneratePW(int(sys.argv[2]), 'very strong')
+                random_generated_password = generate_random_password(int(sys.argv[2]), "very strong")
                 pyperclip.copy(random_generated_password)
                 print(f"{colorama.Fore.GREEN}Password copied!")
             except (ValueError, IndexError):
-                random_generated_password = GeneratePW(20, 'very strong')
+                random_generated_password = generate_random_password(20, "very strong")
                 pyperclip.copy(random_generated_password)
                 print(f"{colorama.Fore.GREEN}Password copied!")
 
+        elif sys.argv[1] == "check":
         # check the strength of the prompted password
-        elif sys.argv[1] == 'check':
-            user_password = input('Enter your password: ')
-            checkPW(user_password)
+            user_password = input("Enter your password: ")
+            check_password(user_password)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
